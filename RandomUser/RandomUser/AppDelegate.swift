e21438770ce0de17503943a5316cdd5a433c9044
Daniel <danielesalinas23@gmail.com>
2021-03-27 16:18:31 -0600

    Start Xcode project

---
+++ RandomUser/RandomUser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RandomUser
+//
+//  Created by Daniel Esteban Salinas on 27/03/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let rootVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "InitialViewController")
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
