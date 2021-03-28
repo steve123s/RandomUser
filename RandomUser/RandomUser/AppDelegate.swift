ae61986cab96d3f5730a2f0a893067aa4b9fbc83
Daniel <danielesalinas23@gmail.com>
2021-03-28 01:14:56 -0600

    Update first load VC

--- RandomUser/RandomUser/AppDelegate.swift
+++ RandomUser/RandomUser/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        let rootVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "InitialViewController")
+        let rootVC = UIStoryboard.loadMainStoryboardController(named: .ProfileNavigationController)
         window?.rootViewController = rootVC
         window?.makeKeyAndVisible()
         
